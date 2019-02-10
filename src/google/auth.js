97ad3a5f0a044c904597548819e5c2158101583c
peter heinum <peter.heinum@gmail.com>
2019-02-10 13:00:33 +0100

    added env variables higher in the auth chain so the credentials string doessn't breake

--- src/google/auth.js
+++ src/google/auth.js
@@ -5,7 +5,11 @@ const readline = require('readline');
 const { google } = require('googleapis');
 
 const TOKEN_PATH = 'src/token.json';
-const credentials = `{"installed":{"client_id":"${process.env.clientID}","project_id":"${process.env.ProjectId}","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"${process.env.ClientSecret}","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`;
+const client_id = process.env.clientID;
+const project_id = process.env.ProjectId;
+const client_secret = process.env.ClientSecret;
+
+const credentials = `{"installed":{"client_id":"${client_id}","project_id":"${project_id}","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"${client_secret}","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`;
 
 const Auth = {
   authorize(credentials, callback) {
