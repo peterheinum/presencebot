e96b76dcf9b652601aba20b29fafb3a3f79a60f2
peter heinum <peter.heinum@gmail.com>
2019-02-10 13:05:25 +0100

    env variable fixing, clientid doesnt get passed to function.. hmm

--- src/google/auth.js
+++ src/google/auth.js
@@ -5,15 +5,16 @@ const readline = require('readline');
 const { google } = require('googleapis');
 
 const TOKEN_PATH = 'src/token.json';
-const client_id = process.env.clientID;
-const project_id = process.env.ProjectId;
-const client_secret = process.env.ClientSecret;
+const CLIENTID = process.env.clientID;
+const PROJECTID = process.env.ProjectId;
+const CLIENTSECRET = process.env.ClientSecret;
 
-const credentials = `{"installed":{"client_id":"${client_id}","project_id":"${project_id}","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"${client_secret}","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`;
+const credentials = `{"installed":{"client_id":"${CLIENTID}","project_id":"${PROJECTID}","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"${CLIENTSECRET}","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`;
 
 const Auth = {
   authorize(credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
+    if(client_id == undefined) client_id = CLIENTID;
     const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0],
     );
